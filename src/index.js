4fe6a5d963d740d7f6e15cf50058071b3a12e44f
Ragum02 <benjamin.souiket@gmail.com>
2024-11-13 13:08:42 +0100

    WIP Project restaurant

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const contentBox = document.querySelector("#content");
+
+const title = document.createElement("h1");
+title.textContent = "My restaurant";
+contentBox.appendChild(title);
